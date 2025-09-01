b756d418fe4c43701c8a3a1eb5432a1b6fd2fb94
tixi333 <tramirezgarin@alumno.huergo.edu.ar>
2025-09-01 08:32:47 -0300

    Update ejemplo_cambiodpantalla.py

--- ejemplo_cambiodpantalla.py
+++ ejemplo_cambiodpantalla.py
@@ -9,13 +9,12 @@ width, height = 600 , 800
 font = pygame.font.Font("monogram-extended.ttf", 100)
 
 text_play = font.render("play screen", True, white)
-play_rect = text_play.get_rect(center = (width //2 , height //2))
 
 text_options = font.render("options screen", True, white)
-options_rect = text_options.get_rect(center = (width //2, height //2))
+
 
 text_credits = font.render("credits screen",True, white)
-credits_rect = credits.get_rect(center = (width //2 , height//2 ))
+
 
 screen = pygame.display.set_caption(width,height)
 screen.fill(black)
