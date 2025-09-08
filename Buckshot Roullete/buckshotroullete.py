c8a9f981bd4931762191da5500a4ad70d37e7190
tixi <tramirezgarin@alumno.huergo.edu.ar>
2025-09-08 10:21:53 -0300

    add blackground images 2

--- Buckshot Roullete/buckshotroullete.py
+++ Buckshot Roullete/buckshotroullete.py
@@ -10,7 +10,69 @@ pygame.display.set_caption("Buckshot Roullete")
 font = pygame.font.Font("monogram-extended.ttf", 40) 
 
 text_enter = font.render("Press enter to start", True, grey)
+# ------------------- load images background gif ----------------------
+zero = pygame.image.load("1.png").convert()
+zero_scale = pygame.transform.scale(zero,(width,height))
 
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+two= pygame.image.load("1.png").convert()
+two_scale = pygame.transform.scale(two,(width,height))
+
+three = pygame.image.load("1.png").convert()
+three_scale = pygame.transform.scale(three,(width,height))
+
+four = pygame.image.load("1.png").convert()
+four_scale = pygame.transform.scale(four,(width,height))
+
+five = pygame.image.load("1.png").convert()
+five_scale = pygame.transform.scale(five,(width,height))
+
+six = pygame.image.load("1.png").convert()
+six_scale = pygame.transform.scale(one,(width,height))
+
+seven = pygame.image.load("1.png").convert()
+seven_scale = pygame.transform.scale(one,(width,height))
+
+eight = pygame.image.load("1.png").convert()
+eight_scale = pygame.transform.scale(one,(width,height))
+
+nine = pygame.image.load("1.png").convert()
+nine_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
+
+one = pygame.image.load("1.png").convert()
+one_scale = pygame.transform.scale(one,(width,height))
 class Button:
     def __init__(self):
         pass
@@ -33,8 +95,10 @@ while running:
     if actual_screen == "main_screen":
         screen.fill(black)
         screen.blit(text_enter,(250,500))
+
     elif actual_screen == "menu_screen":
         screen.fill(grey)
+        screen.blit(tri_scale, (0,0))
     pygame.display.flip()
 
 pygame.quit()
