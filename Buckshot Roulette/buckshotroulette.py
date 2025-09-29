7b978c7451e6b5aa83f6849fd9eadfdc8afb2e4f
Agustin Granes <tramirezgarin@alumno.huergo.edu.ar>
2025-09-29 10:33:35 -0300

    images

--- Buckshot Roulette/buckshotroulette.py
+++ Buckshot Roulette/buckshotroulette.py
@@ -13,6 +13,7 @@ pygame.display.set_caption("Buckshot Roullete")
 
 fontr = pygame.font.Font("EBGaramond-VariableFont_wght.ttf", 100)
 #----------------- FONT
+font0 = pygame.font.Font("monogram-extended.ttf",20)
 font = pygame.font.Font("monogram-extended.ttf", 40) 
 font2 = pygame.font.Font("monogram-extended.ttf", 50)
 #----------------------- menu
@@ -37,6 +38,9 @@ text_hard_d = font.render("Hard mode description", True, red)
 text_volumen = font.render("Volumen",True, grey)
 text_options_difficulty = font.render("Modo de dificultad",True,grey)
 
+easy = font0.render("descripcion easy", True, grey)
+normal = font0.render("descripcion normal", True, grey)
+hard = font0.render("descripcion hard", True, grey)
 #----------------- return text
 text_general_return = font.render("Press return to go back", True,grey)
 text_enter = font.render("Press enter to start", True, grey)
@@ -64,99 +68,87 @@ text_hard = font.render("Hard", True, red)
 text_hard2 = font.render("Hard", True, grey)
 
 # ------------------- load images background gif ----------------------
-zero = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/0.png").convert()
+zero = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/0.png").convert()
 zero_scale = pygame.transform.scale(zero,(width,height))
 
-one = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/1.png").convert()
+one = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/1.png").convert()
 one_scale = pygame.transform.scale(one,(width,height))
 
-two= pygame.image.load("Buckshot Roullete/background_buckshot_roullete/2.png").convert()
+two= pygame.image.load("Buckshot Roulette/background_buckshot_roulette/2.png").convert()
 two_scale = pygame.transform.scale(two,(width,height))
 
-three = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/3.png").convert()
+three = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/3.png").convert()
 three_scale = pygame.transform.scale(three,(width,height))
 
-four = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/4.png").convert()
+four = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/4.png").convert()
 four_scale = pygame.transform.scale(four,(width,height))
 
-five = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/5.png").convert()
+five = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/5.png").convert()
 five_scale = pygame.transform.scale(five,(width,height))
 
-six = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/6.png").convert()
+six = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/6.png").convert()
 six_scale = pygame.transform.scale(six,(width,height))
 
-seven = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/7.png").convert()
+seven = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/7.png").convert()
 seven_scale = pygame.transform.scale(seven,(width,height))
 
-eight = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/8.png").convert()
+eight = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/8.png").convert()
 eight_scale = pygame.transform.scale(eight,(width,height))
 
-nine = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/9.png").convert()
+nine = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/9.png").convert()
 nine_scale = pygame.transform.scale(nine,(width,height))
 
-ten = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/10.png").convert()
+ten = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/10.png").convert()
 ten_scale = pygame.transform.scale(ten,(width,height))
 
-eleven = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/11.png").convert()
+eleven = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/11.png").convert()
 eleven_scale = pygame.transform.scale(eleven,(width,height))
 
-twelve = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/12.png").convert()
+twelve = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/12.png").convert()
 twelve_scale = pygame.transform.scale(twelve,(width,height))
 
-thirteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/13.png").convert()
+thirteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/13.png").convert()
 thirteen_scale = pygame.transform.scale(thirteen,(width,height))
 
-fourteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/14.png").convert()
+fourteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/14.png").convert()
 fourteen_scale = pygame.transform.scale(fourteen,(width,height))
 
-fifteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/15.png").convert()
+fifteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/15.png").convert()
 fifteen_scale = pygame.transform.scale(fifteen,(width,height))
 
-sixteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/16.png").convert()
+sixteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/16.png").convert()
 sixteen_scale = pygame.transform.scale(sixteen,(width,height))
 
-seventeen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/17.png").convert()
+seventeen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/17.png").convert()
 seventeen_scale = pygame.transform.scale(seventeen,(width,height))
 
-eighteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/18.png").convert()
+eighteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/18.png").convert()
 eighteen_scale = pygame.transform.scale(eighteen,(width,height))
 
-nineteen = pygame.image.load("Buckshot Roullete/background_buckshot_roullete/19.png").convert()
+nineteen = pygame.image.load("Buckshot Roulette/background_buckshot_roulette/19.png").convert()
 nineteen_scale = pygame.transform.scale(nineteen,(width,height))
-
-#------------------------------ play - cambiar
-#play1 = pygame.image.load("playbr1.png").convert()
-#play2 = pygame.image.load("playbr2.png").convert()
-
-#play1_scale = pygame.transform.scale(play1, (175,75))
-#play2_scale = pygame.transform.scale(play2, (175,75))
-
-#------------------------------ options - cambiar
-#options1 = pygame.image.load("options br1.png").convert()
-#options2 = pygame.image.load("options br2.png").convert()
-
-#options1_scale = pygame.transform.scale(options1,(175,75))
-#options2_scale = pygame.transform.scale(options2,(175,75))
 
 # ---------------------------
 class Button:
-    def __init__(self,image1,image2,x,y,action,):
+    def __init__(self,image1,image2,x,y,action,function,text):
         self.image1 = image1
         self.image2 = image2
-        self.image = self.image1
+        self.image = self.image1 
         self.x = x
         self.y = y
         self.action = action
         self.rect = self.image1.get_rect(center = (self.x,self.y))
         self.hovered= False
-        #self.color1 = color1
-        #self.color = color2
+        self.function = function
+        self.text = text
 
     def draw_button(self, surface):
         surface.blit(self.image, self.rect)
         if self.hovered== True:
-
-            pygame.draw.circle(surface, grey, (self.rect.left - 10, self.rect.centery ), 5)
+            if self.function == "menu":
+                pygame.draw.circle(surface, grey, (self.rect.left - 10, self.rect.centery ), 5)
+        if self.text != False:
+            surface.blit(self.text, (200, 160))
 
     def handle_event(self,event):
         if event.type == pygame.MOUSEMOTION:
@@ -171,13 +163,13 @@ class Button:
             if self.rect.collidepoint(event.pos):
                 self.action()
                 
-class Button_activate:
-    def __init__(self,color1,color2,x,y,width,height):
+class Activate:
+    def __init__(self,color1,color2,x,y,width,height,current_mode):
         self.color1 = color1
         self.color2 = color2
         self.state = False
         self.rect = pygame.Rect(x,y,width,height)
-
+        self.current = current_mode
     def draw_button(self,surface):
         if self.state == True:
             self.actual_color = self.color2
@@ -185,22 +177,22 @@ class Button_activate:
             self.actual_color = self.color1
         
         pygame.draw.rect(surface, self.actual_color, self.rect)
-
-    def handle_event(self,button_activate, event):
+        if self.state == True:
+            screen.blit(self.current, (60,400))
+    def handle_event(self,button_activate,event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.rect.collidepoint(event.pos):
-                if button_activate:
-                    for button in button_activate:
-                        button.state = False
+                for button in button_activate:
+                    button.state = False
                 self.state = True
-                return True
-        return False
+            else:
+                self.state = False
     
 
 button_activate = [
-    Button_activate(red,grey,150, 255, 30, 30),
-    Button_activate(red, grey, 150, 305, 30, 30),  
-    Button_activate(red, grey, 150, 355, 30, 30)
+    Activate(red,grey,150, 255, 30, 30,easy),
+    Activate(red, grey, 150, 305, 30, 30,normal),  
+    Activate(red, grey, 150, 355, 30, 30,hard)
 ]
 #---------- functions
 
@@ -234,26 +226,17 @@ def show_current_difficulty(difficulty):
 
 #----------- button
 buttons_menu = [
-    Button(text_play,text_play2,90,250, show_play_screen),
-    Button(text_options,text_options2,115,300,show_options_screen),
-    Button(text_how2play,text_how2play2,155,350,show_how2play_screen),
+    Button(text_play,text_play2,90,250, show_play_screen,"menu",False),
+    Button(text_options,text_options2,115,300,show_options_screen,"menu",False),
+    Button(text_how2play,text_how2play2,155,350,show_how2play_screen,"menu",False)
     ]
 
 buttons_options = [
-    Button(text_easy,text_easy2,60,265, show_current_difficulty("easy")),
-    Button(text_normal,text_normal2,60,315, show_current_difficulty("normal")),
-    Button(text_hard,text_hard2,60,365, show_current_difficulty("hard")),
-    Button(text_easy_d,text_easy_d2,400,265, show_options_easy),
-    Button(text_normal_d,text_normal_d2,400,315, show_options_normal),
-    Button(text_hard_d,text_hard_d2,400,365, show_options_hard),
+    Button(text_easy_d,text_easy_d2,400,265, show_options_easy, "options",easy),
+    Button(text_normal_d,text_normal_d2,400,315, show_options_normal, "options",normal),
+    Button(text_hard_d,text_hard_d2,400,365, show_options_hard, "options",hard),
     ]
 
-    #Button(hard1_scale,hard2_scale, width//2,400, show_options_mode)
-    #Button(easy1_scale,easy2_scale, widht//2,400, show_options_mode)
-    #Button(normal1_scale,normal2_scale,width//2,400, show_options_mode)
-    #Button(play1_scale,play2_scale,100,250, show_play_screen),
-    #Button(options1_scale,options2_scale,100,400, show_options_screen)
-    # añadir a buttons cuando esten cargadas las imagenes
 #------------- background images
 
 background_frames = [
@@ -295,6 +278,9 @@ while running:
                 b_selected = False
 
                 button.handle_event(event)
+
+            for button in button_activate:
+                button.handle_event(button_activate, event)
 
         elif actual_screen == "how2play_screen":
             if event.type == pygame.KEYDOWN:
@@ -344,12 +330,17 @@ while running:
         screen.blit(text_options_difficulty, (50,200))
         screen.blit(text_volumen, (50,100))
         screen.blit(text_options, (200,20))
-
+        screen.blit(text_easy,(60,265))
+        screen.blit(text_normal,(60,315))
+        screen.blit(text_hard,(60,365))
         pygame.draw.line(screen, grey, (50,150), (700,150), 2)
         pygame.draw.line(screen, grey, (50,240), (700,240), 2)
         pygame.draw.line(screen, grey, (50,400), (700,400), 2)
 
         for button in buttons_options:
+            button.draw_button(screen)
+        
+        for button in button_activate:
             button.draw_button(screen)
     
     elif actual_screen == "how2play_screen":   #screen
